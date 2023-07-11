32e822e102d8e8599a72290b184129228d2ca22e
Hexenwake <69510780+Hexenwake@users.noreply.github.com>
2023-07-11 13:14:37 +0800

    Update module.py

--- latest/module.py
+++ latest/module.py
@@ -5,7 +5,6 @@ from collections import Counter
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(
     static_image_mode=False,
-    model_complexity=1,
     min_detection_confidence=0.75,
     min_tracking_confidence=0.75,
     max_num_hands=2
